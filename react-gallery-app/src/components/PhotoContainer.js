a9519c1bc0d84e826a2cc886e9f960aa08b245c3
Trevor <trevorwerner@hotmail.com>
2021-09-28 00:59:08 -0400

    Loading preset data, begin routes

---
+++ react-gallery-app/src/components/PhotoContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class PhotoContainer extends Component {
+   render() {
+      return (
+         <div className="photo-container">
+            <h2>Results</h2>
+            <ul>
+
+            </ul>
+         </div>
+      )
+   }
+}
+
+export default PhotoContainer;
